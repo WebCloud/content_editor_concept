98266bc806a2b877ce5e61941dfaed3cb1021a9f
Vinicius Dallacqua <vinicius.dallacqua@bonniergaming.com>
2016-02-11 19:24:16 +0100

    Change the TextPlugin to use React

--- src/js/parser/plugins/text-plugin.js
+++ src/js/parser/plugins/text-plugin.js
@@ -1,14 +1,47 @@
-import PluginFactory from './plugin-factory';
+import React, { Component, PropTypes } from 'react';
 
-const style = `
-<style>
-  .text-plugin {
-    padding: 1em;
-    border: dashed 2px #acacac;
+const style = {
+  padding: '1em',
+  border: 'dashed 2px #acacac'
+};
+
+function clickEventHandler() {
+  this.setState({ editMode: !this.state.editMode })
+}
+
+export default class TextPlugin extends Component {
+  static propTypes = {
+    className: PropTypes.string
+  };
+
+  constructor(props) {
+    super(props);
+    this.clickEventHandler = clickEventHandler.bind(this);
+    this.state = { text: 'Here will be some text', editMode: false };
   }
-</style>`;
 
-const markup = '<div class="text-plugin">Here will be some text</div>';
-// const pluginEvents = [{ eventName: 'click', eventHander: ()=> console.log('called') }];
+  parseContent(classNames) {
+    let parsedContent = (
+      <h1 style={style} className={classNames} onClick={this.clickEventHandler}>
+        { this.state.text }
+      </h1>
+    );
 
-export default new PluginFactory({ name: 'text', markup, style });
+    if (this.state.editMode) {
+      parsedContent = (
+        <input type="text" placeholder="Here will be some text" />
+      );
+    }
+
+    return parsedContent;
+  }
+
+  render() {
+    const { className = '' } = this.props;
+    const classNames = `text-plugin ${className}`;
+
+    return (<div>
+      {this.parseContent(classNames)}
+    </div>);
+  }
+}
