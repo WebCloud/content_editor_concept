eddc06ee3d1917d6114b7919294d6fa22fe12e19
Vinicius Dallacqua <vinicius.dallacqua@bonniergaming.com>
2016-02-17 15:43:23 +0100

    Update content-editor for preview toggling
    Now the preview button will toggle between preview mode and edit mode

--- src/js/content-editor.js
+++ src/js/content-editor.js
@@ -6,11 +6,40 @@ export default class ContentEditor extends Component {
     previewHandler: PropTypes.func
   };
 
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { isPreviewing: false, previewContent: '' };
+  }
+
+  getInnerHTML() {
+    const html = this.props.previewHandler();
+    return {
+      __html: html
+    };
+  }
+
+  handleClick() {
+    this.setState({ isPreviewing: !this.state.isPreviewing });
+  }
+
+  buildContent() {
+    let defaultContent = this.props.children;
+
+    if (this.state.isPreviewing) {
+      defaultContent = (
+        <div dangerouslySetInnerHTML={this.getInnerHTML()}></div>
+      );
+    }
+    return defaultContent;
+  }
+
   render() {
     return (
       <div>
-        <button onClick={ this.props.previewHandler }>Preview</button> <br />
-        {this.props.children}
+        <button onClick={ this.handleClick }>Toggle Preview</button> <br />
+        {this.buildContent()}
       </div>
     );
   }
