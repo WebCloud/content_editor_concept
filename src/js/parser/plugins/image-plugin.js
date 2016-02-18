a5150962daa1701bd3e9c8ab4333600a20ba9f7f
Vinicius Dallacqua <vinicius.dallacqua@bonniergaming.com>
2016-02-18 19:47:14 +0100

    Update ImagePlugin to become a React.Component
    Now the ImagePlugin follows a similar, but improved, design to the
    TextPlugin. It has already some functions for the intended drag and drop
    and file upload functions, to be fully implemented on a future branch.
    
    It will take an image url and use it as the src. The improvements are
    where the component manages it's style with the isPreviewing prop to
    eliminate the borders and show a cleaner slate.

--- src/js/parser/plugins/image-plugin.js
+++ src/js/parser/plugins/image-plugin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 
 const style = {
   padding: '1em',
@@ -7,18 +7,144 @@ const style = {
   margin: '0em 1em 1em 0em'
 };
 
-function clickEventHandler() {
-  console.info('clicked');
-}
+export default class ImagePlugin extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    width: PropTypes.string,
+    height: PropTypes.string,
+    pluginIndex: PropTypes.number,
+    getMarkdown: PropTypes.func,
+    isPreviewing: PropTypes.bool
+  };
 
-export default function ImagePlugin(props) {
-  const { className = '', width = 'auto', height = width, getMarkdown, pluginIndex } = props;
-  const pluginStyle = Object.assign({}, style, { width, height });
-  const classNames = `image-plugin ${className}`;
+  constructor(props) {
+    super(props);
+    this.clickHandler = this.clickHandler.bind(this);
+    this.handleImageExtraction = this.handleImageExtraction.bind(this);
 
-  getMarkdown('![deadpool](http://i.imgur.com/wXpNi4T.gif)', pluginIndex);
+    this.state = {
+      editMode: false,
+      imageURL: 'http://i.imgur.com/wXpNi4T.gif'
+    };
+  }
 
-  return (<div style={pluginStyle} className={classNames} onClick={clickEventHandler}>
-    Here will be some image
-  </div>);
+  handleImageExtraction(event) {
+    const { target } = event;
+    event.preventDefault();
+    event.stopPropagation();
+
+    const inputs = target.parentElement.querySelectorAll('input');
+    let element = null;
+
+    inputs.forEach((input) => {
+      if (input.value !== '' && input.value !== null) {
+        element = input;
+        return;
+      }
+    });
+
+    if (element.type === 'file') {
+      if (!element.name.match(/.jpg|.png|.gif/)) return;
+
+      // send over the file
+    } else {
+      if (!element.value.match(/.jpg|.png|.gif/)) return;
+
+      const imageURL = element.value;
+      this.setState({ imageURL, editMode: false });
+    }
+  }
+
+  dragOver({ preventDefault }) {
+    // this is needed to avoid the default behaviour from the browser
+    preventDefault();
+  }
+
+  dragEnter({ preventDefault }) {
+    preventDefault();
+    this.setState({ isDragging: true });
+  }
+
+  dragLeave({ preventDefault }) {
+    preventDefault();
+    this.setState({ isDragging: false });
+  }
+
+  drop({ preventDefault, dataTransfer: { files } }) {
+    if (!this.state.isDisabled) {
+      preventDefault();
+      this.setState({ isDragging: false });
+
+      // only 1 file for now
+      const file = files[0];
+      this.setState({ isDisabled: true });
+      console.info(file);
+    } else {
+      console.error('you can only upload on file at the time');
+    }
+  }
+
+  clickHandler() {
+    this.setState({ editMode: true });
+  }
+
+  buildContent(isPreviewing) {
+    const { imageURL } = this.state;
+    const imageStyle = { display: 'block', width: this.props.width };
+    const imageElement = <img src={imageURL} style={ imageStyle } />;
+
+    let defaultContent = (
+      <div>
+        Here will be an image like bellow
+        { imageElement }
+      </div>
+    );
+
+    if (this.state.editMode) {
+      const editorStyle = {
+        display: 'flex',
+        flexDirection: 'column'
+      };
+
+      defaultContent = (
+        <div style={ editorStyle } className="image-editor-wrapper">
+          <input type="text" placeholder="http://image.url" />
+          <br />
+          <span>or</span>
+          <br />
+          <input type="file" />
+          <br />
+          <button onClick={this.handleImageExtraction}>Done</button>
+        </div>
+      );
+    }
+
+    if (isPreviewing) {
+      defaultContent = imageElement;
+    }
+
+    return defaultContent;
+  }
+
+  render() {
+    const {
+      className = '',
+      getMarkdown,
+      pluginIndex,
+      isPreviewing
+    } = this.props;
+    const classNames = `image-plugin ${className}`;
+    const markdown = `![](${this.state.imageURL})`;
+    const pluginStyle = Object.assign({}, style, {
+      border: ((isPreviewing) ? 'none' : style.border)
+    });
+
+    getMarkdown({ markdown, pluginIndex });
+
+    return (
+      <div style={pluginStyle} className={classNames} onClick={this.clickHandler}>
+        {this.buildContent(isPreviewing)}
+      </div>
+    );
+  }
 }
