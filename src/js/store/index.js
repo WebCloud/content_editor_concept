b7ccc19ff035874a032f865e770315ea5c186a5a
Vinicius Dallacqua <vinicius.dallacqua@bonniergaming.com>
2016-03-02 19:12:11 +0100

    Create the store
    The store is a factory that will return an object to be used to persist
    data. For this initial implementation we have the save method only.
    
    The store factory will take an adapter. Which will consist of an object
    that will provide the store with the desired persistence implementation.
    It consists of a normalizer function, serializer funtion and a config
    object.
    
    By default the store comes with a dropbox and HTTP adapter. The dropbox
    adapter is still on experimental stages. The HTTP adapter works with the
    server's asset API endpoint, and is the default adapter to be used.

---
+++ src/js/store/index.js
@@ -0,0 +1,34 @@
+const NO_DATA = 'No data to send';
+
+function httpSave({ data, normalizer, pluginId, config }) {
+  return new Promise((resolve, reject) => {
+    if (data === null) reject({ cause: NO_DATA });
+    const { endpoint, ...settings } = config;
+    settings.body = data;
+
+    fetch(endpoint, settings)
+      .then(normalizer)
+      .then((response) => {
+        const json = response;
+        json.pluginId = json.pluginId || pluginId;
+        resolve(json);
+      })
+      .catch((reason) => (reject(reason)));
+  });
+}
+
+function store(adapter) {
+  function save(rawData) {
+    const { save: { config, serializer, normalizer } } = adapter;
+    return serializer(rawData)
+      .then(({ data, pluginId }) => httpSave({ data, normalizer, pluginId, config }));
+  }
+
+  return {
+    save
+  };
+}
+
+export {
+  store
+};
