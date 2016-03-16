5fb2b41553d1e9af8d30f99a3d3ff7432fa18778
Vinicius Dallacqua <vinicius.dallacqua@bonniergaming.com>
2016-03-16 17:24:03 +0100

    Add a higher order component for better composition
    Add the PluginConstructor in order to create a higher order component
    composition. This allow a transparent layer with the common logic
    between plugins, which would be injected into the plugin instances. Also
    allows for simpler plugins to be pure functions, which is a performance
    gain.

---
+++ src/js/parser/plugins/plugin-constructor.js
@@ -0,0 +1,57 @@
+import React, { Component, PropTypes } from 'react';
+import { autobind } from 'core-decorators';
+
+export default function PluginConstructor(Plugin) {
+  return class extends Component {
+    static propTypes = {
+      className: PropTypes.string,
+      pluginIndex: PropTypes.number,
+      setPluginData: PropTypes.func,
+      isPreviewing: PropTypes.bool,
+      pluginId: PropTypes.string
+    };
+
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        editMode: false,
+        pluginData: { markdown: '' }
+      };
+    }
+
+    @autobind
+    toggleEditMode() {
+      this.setState({ editMode: !this.state.editMode });
+    }
+
+    @autobind
+    updatePluginData({ pluginData, editMode = this.state.editMode }) {
+      this.setState({ pluginData, editMode });
+    }
+
+    render() {
+      const style = {
+        padding: '1em',
+        border: 'dashed 2px #acacac',
+        display: 'inline-block'
+      };
+
+      const pluginStyle = Object.assign({}, style, {
+        border: ((this.props.isPreviewing) ? 'none' : style.border)
+      });
+
+      const { pluginIndex, pluginId } = this.props;
+      const { pluginData } = this.state;
+
+      this.props.setPluginData({ pluginData, pluginIndex, pluginId });
+      return (<Plugin
+        {...this.props}
+        {...this.state}
+        toggleEditMode={this.toggleEditMode}
+        updatePluginData={this.updatePluginData}
+        style={pluginStyle}
+      />);
+    }
+  };
+}
